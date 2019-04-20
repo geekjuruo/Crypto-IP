983bdcc439b35d45fce30e5438739ce41ed6706a
liyinghui <li-yh16@mails.tsinghua.edu.cn>
2019-04-21 00:21:06 +0800

    change C to CPP

--- Crypto-PAn.1.0/sample.cpp
+++ Crypto-PAn.1.0/sample.cpp
@@ -33,24 +33,24 @@ int main(int argc, char * argv[]) {
     }
        
     //readin and handle each line of the input file
-    while  (fscanf(f, "%f", &packet_time) != EOF) {
-	fscanf(f, "%u", &packet_size);
-	fscanf(f, "%u.%u.%u.%u", &packet_addr1, &packet_addr2, &packet_addr3, &packet_addr4);
+    while  (fscanf(f, "%u.%u.%u.%u", &packet_addr1, &packet_addr2, &packet_addr3, &packet_addr4) != EOF) {
+      // fscanf(f, "%u", &packet_size);
+      // fscanf(f, "%u.%u.%u.%u", &packet_addr1, &packet_addr2, &packet_addr3, &packet_addr4);
 
-	//convert the raw IP from a.b.c.d format into unsigned int format.
-	raw_addr = (packet_addr1 << 24) + (packet_addr2 << 16) + (packet_addr3 << 8) + packet_addr4;
+      //convert the raw IP from a.b.c.d format into unsigned int format.
+      raw_addr = (packet_addr1 << 24) + (packet_addr2 << 16) + (packet_addr3 << 8) + packet_addr4;
 
-	//Anonymize the raw IP
-	anonymized_addr = my_anonymizer.anonymize(raw_addr);
+      //Anonymize the raw IP
+      anonymized_addr = my_anonymizer.anonymize(raw_addr);
 
-	//convert the anonymized IP from unsigned int format to a.b.c.d format
-	packet_addr1 = anonymized_addr >> 24;
-	packet_addr2 = (anonymized_addr << 8) >> 24;
-	packet_addr3 = (anonymized_addr << 16) >> 24;
-	packet_addr4 = (anonymized_addr << 24) >> 24;
+      //convert the anonymized IP from unsigned int format to a.b.c.d format
+      packet_addr1 = anonymized_addr >> 24;
+      packet_addr2 = (anonymized_addr << 8) >> 24;
+      packet_addr3 = (anonymized_addr << 16) >> 24;
+      packet_addr4 = (anonymized_addr << 24) >> 24;
 
-	//output the sanitized trace
-	printf("%6f\t%u\t%u.%u.%u.%u\n",  packet_time, packet_size, packet_addr1, packet_addr2, packet_addr3, packet_addr4 );
+      //output the sanitized trace
+      printf("%6f\t%u\t%u.%u.%u.%u\n",  packet_time, packet_size, packet_addr1, packet_addr2, packet_addr3, packet_addr4 );
     }
 
 }
